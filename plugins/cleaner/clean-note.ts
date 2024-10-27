d3fb1a23eace473fe8e33a07c4c909b80c9bad81
Landon Schropp <schroppl@gmail.com>
2024-10-27 14:32:41 -0700

    wip

--- plugins/cleaner/clean-note.ts
+++ plugins/cleaner/clean-note.ts
@@ -17,6 +17,23 @@ const WHITESPACE_REGEX = /^[ \t]+$/gm;
 const NEWLINES_REGEX = /\n{3,}/g;
 
 /**
+ * This function repeatedly runs a transform function until there are no more matches.
+ * @
+ */
+function runTransformUntilNoChange(
+  content: string,
+  transform: (content: string) => string,
+): string {
+  const transformedContent = transform(content);
+
+  if (transformedContent === content) {
+    return transformedContent;
+  }
+
+  return runTransformUntilNoChange(transformedContent, transform);
+}
+
+/**
  * Cleans the provided note.
  *
  * This is _not_ meant to be perfect. There are likely edge cases that this function does not
@@ -29,12 +46,17 @@ const NEWLINES_REGEX = /\n{3,}/g;
 export async function cleanNote(app: App, file: TFile): Promise<void> {
   const content = await app.vault.read(file);
 
-  const replacementContent = content
-    .replace(COMMENT_REGEX, "")
-    .replace(EMPTY_LIST_ITEM_REGEX, "")
-    .replace(EMPTY_SECTION_REGEX, "")
-    .replace(WHITESPACE_REGEX, "")
-    .replace(NEWLINES_REGEX, "\n\n");
+  // Keep rerunning the cleaning algorithm until no more changes are available. This is necessary to
+  // deal with tricky cases like nested empty headers, and can't be solved by running the regular
+  // expression in reverse because JavaScript doesn't support that.
+  const replacementContent = runTransformUntilNoChange(content, (content) =>
+    content
+      .replace(COMMENT_REGEX, "")
+      .replace(EMPTY_LIST_ITEM_REGEX, "")
+      .replace(EMPTY_SECTION_REGEX, "")
+      .replace(WHITESPACE_REGEX, "")
+      .replace(NEWLINES_REGEX, "\n\n"),
+  );
 
   await app.vault.modify(file, replacementContent);
 }

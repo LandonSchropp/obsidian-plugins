1c736e069cef4ad3cace4e019443e4dc558192a5
Landon Schropp <schroppl@gmail.com>
2024-11-01 16:34:17 -0700

    Add convertToKebabCase

--- shared/string.ts
+++ shared/string.ts
@@ -7,3 +7,12 @@
 export function pluralize(word: string, count: number) {
   return count === 1 ? word : `${word}s`;
 }
+
+/**
+ * Converts a string to kebab-case.
+ * @param text The text to convert.
+ * @returns The kebab-cased text.
+ */
+export function convertToKebabCase(text: string) {
+  return text.replace(/\s+/g, "-").toLowerCase();
+}

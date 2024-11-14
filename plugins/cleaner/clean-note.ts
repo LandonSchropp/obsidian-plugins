387acb5fcc1a17ecafcd017775eb28758042add5
Landon Schropp <schroppl@gmail.com>
2024-11-13 21:17:13 -0800

    Add custom regular expressions to remove

--- plugins/cleaner/clean-note.ts
+++ plugins/cleaner/clean-note.ts
@@ -21,6 +21,21 @@ const WHITESPACE_REGEX = /^[ \t]+$/gm;
 // Matches more than two newlines.
 const NEWLINES_REGEX = /\n{3,}/g;
 
+// Removes the "Five Minute Journal" sections from my daily note when they're empty.
+const CUSTOM_REGEXES = [
+  /I am grateful for…\s+1.\s+2.\s+3.\s*\n/,
+  /What would make today great\?\s+1.\s+2.\s+3.\s*\n/,
+];
+
+// All of the regular expression to remove in the note.
+const REMOVAL_REGEXES = [
+  COMMENT_REGEX,
+  EMPTY_LIST_ITEM_REGEX,
+  ...CUSTOM_REGEXES,
+  ...EMPTY_SECTION_REGEXES,
+  WHITESPACE_REGEX,
+];
+
 /**
  * This is similar to the built-in `replaceAll` function, expected it repeatedly reruns the regular
  * expression until it no longer matches.
@@ -48,16 +63,12 @@ function replaceAll(content: string, regex: RegExp, replacement: string): string
 export async function cleanNote(app: App, file: TFile): Promise<void> {
   let content = await app.vault.read(file);
 
-  // Replace all occurrences of the provided regular expressions. Using `replaceAll` here handles
-  // some tricky regular expression nesting cases, such as nested empty headers.
-  content = replaceAll(content, COMMENT_REGEX, "");
-  content = replaceAll(content, EMPTY_LIST_ITEM_REGEX, "");
-
-  for (const regex of EMPTY_SECTION_REGEXES) {
+  // Replace all occurrences of the provided regular expressions.
+  for (const regex of REMOVAL_REGEXES) {
     content = replaceAll(content, regex, "");
   }
 
-  content = replaceAll(content, WHITESPACE_REGEX, "");
+  // Replace all instances of more than two newlines with two
   content = replaceAll(content, NEWLINES_REGEX, "\n\n");
 
   await app.vault.modify(file, content);
